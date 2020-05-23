1e43972b9031b857994a3a425b6958ced9495162
johnthomson91 <63977222+johnthomson91@users.noreply.github.com>
2020-05-23 01:23:49 -0400

    Add files via upload

--- PyBank/main_PyBank.py
+++ PyBank/main_PyBank.py
@@ -9,9 +9,11 @@ months = []
 with open(bank_csv) as csv_file:
 
     csv_reader = csv.reader(csv_file, delimiter=',')
+    # Create header
     header = next(csv_reader)
 
     for row in csv_reader:
+        # fill the lists created for profits and months
         profits.append(int(row[1]))
         months.append(row[0])
 
@@ -31,16 +33,16 @@ decreaseMonth = (months[decreaseIndex + 1])
 
 month_count = len(months)
 
-
+# Print Results
 print("Financial Analysis")
 print("------------------------")
 print(f"Total Months: {month_count}")
 print(f"Total Profits: ${sumProfit}")
 print(f"Average Change: ${avgChange}")
 print(f"Greatest Increase in Profits: {increaseMonth} ${greatestIncrease}")
-print(f"Greatest Decrese in Profits: {decreaseMonth} ${greatestDecrease}")
+print(f"Greatest Decrease in Profits: {decreaseMonth} ${greatestDecrease}")
 
-
+# Output to txt file
 PyBank = open("PyBank.txt", "w")
 
 PyBank.writelines("Financial Analysis")
@@ -49,4 +51,4 @@ PyBank.writelines(f"\nTotal Months: {month_count}")
 PyBank.writelines(f"\nTotal Profits: ${sumProfit}")
 PyBank.writelines(f"\nAverage Change: ${avgChange}")
 PyBank.writelines(f"\nGreatest Increase in Profits: {increaseMonth} ${greatestIncrease}")
-PyBank.writelines(f"\nGreatest Decrese in Profits: {decreaseMonth} ${greatestDecrease}")
+PyBank.writelines(f"\nGreatest Decrease in Profits: {decreaseMonth} ${greatestDecrease}")
